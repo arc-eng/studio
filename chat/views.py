8fdbf8635abf807b56e08be6e5f26acd41c5ba37
Marco Lamina <mlamina09@gmail.com>
2024-10-01 15:54:25 -0700

    Make sure users can only see their own chats

--- chat/views.py
+++ chat/views.py
@@ -21,16 +21,20 @@ def home(request):
 @login_required
 @needs_api_key
 def view_chat(request, chat_id, api_key):
+    user_chats = ChatConversation.objects.filter(user=request.user)
     if request.method == "POST":
         message = request.POST.get("message")
         if not message:
             raise ValueError("Message is required.")
         else:
-            chat = ChatConversation.objects.get(pk=chat_id)
+            chat = user_chats.get(pk=chat_id)
             chat.continue_conversation(message, api_key)
             return redirect("view_chat", chat_id=chat_id)
 
-    chat = ChatConversation.objects.get(pk=chat_id)
+    try:
+        chat = user_chats.get(pk=chat_id)
+    except ChatConversation.DoesNotExist:
+        return redirect("start_conversation")
     messages = chat.messages.all()
     for message in messages:
         if not message.result:
@@ -47,7 +51,7 @@ def view_chat(request, chat_id, api_key):
             message.result = markdown.markdown(message.result, extensions=['nl2br', 'fenced_code', 'extra'])
         message.message = markdown.markdown(message.message, extensions=['nl2br', 'fenced_code', 'extra'])
     return render(request, "view_chat.html", {
-        "chats": ChatConversation.objects.order_by('-id').all(),
+        "chats": user_chats.order_by('-id').all(),
         "selected_chat": chat,
         "messages": messages,
         "active_tab": "chat",
@@ -74,13 +78,13 @@ def start_conversation(request, api_key):
 
     return render(request, "start_conversation.html", {
         "bookmarked_repos": BookmarkedRepo.objects.all(),
-        "chats": ChatConversation.objects.order_by('-id').all(),
+        "chats": ChatConversation.objects.filter(user=request.user).order_by('-id').all(),
         "active_tab": "chat",
    })
 
 
 @login_required
 def delete_chat(request, chat_id):
-    chat = ChatConversation.objects.get(pk=chat_id)
+    chat = ChatConversation.objects.filter(user=request.user).get(pk=chat_id)
     chat.delete()
     return redirect("start_conversation")
