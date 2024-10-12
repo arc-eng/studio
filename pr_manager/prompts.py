dc8fd44e97fd0a2c9f0f9b57d48ec48acb16e262
Marco Lamina <mlamina09@gmail.com>
2024-10-12 11:41:27 -0700

    The last of it

--- pr_manager/prompts.py
+++ pr_manager/prompts.py
@@ -1,7 +1,7 @@
 PR_DESCRIPTION = """
 I've made some changes and opened a new PR: #{pr_number}.
 
-I need a PR title and a description that summarizes these changes in {structure}.
+I need a PR title and a description that summarizes these changes.
 The PR description will also be used as merge commit message, so it should be clear and informative.
 
 Use the following guidelines:
@@ -13,23 +13,49 @@ Use the following guidelines:
 ## PR Body
 
 - At the very top, provide short paragraph summarizing the changes and their impact.
-- Below, list the changes made in bullet points.
-- Use bold text instead of sections/sub-sections to separate the bullet points into topics
-- There should be no more than 3-4 topics
 - Use the present tense and the active voice.
 - Be specific - Include names, paths, identifiers, names, versions, etc
 - Where possible, make file names/paths clickable using Markdown links. Use this format for the URL: `https://github.com/<github_project>/blob/<pr_branch>/<file_path>`
-
-
-# Your task
-Edit PR #{pr_number} title and description to reflect the changes made in this PR.
-Respond in the following format:
-
-```
-I've updated the title and description of <number and title of PR, hyperlink>
+- Use bold text instead of sections/sub-sections
+- {structure}
+- {emojis}
+- {style}
+{additional_instructions}
 
 ---
 
-<pr description>
-```
+# Your task
+Edit PR #{pr_number} title and description to reflect the changes made in this PR.
+Respond only with a copy of the PR description, nothing else.
 """
+
+
+def structure_prompt(form_select):
+    if form_select == "topics":
+        return ("List the changes made in bullet points and separate them into sections."
+                "Each section should be a topic / category related to the file changes."
+                "There should be no more than 3-4 topics.")
+    elif form_select == "files":
+        return "List the changes made in bullet points and group the changes by file."
+    else:
+        return "Explain the file changes in prose and paragraphs."
+
+def emoji_prompt(form_select):
+    if form_select == "readability":
+        return "Add emojis, to enhance the readability. Use UTF-8 encoding for emojis"
+    elif form_select == "readability_and_fun":
+        return "Add lots of emojis to enhance readability and for visual eye candy and fun! Use UTF-8 encoding for emojis"
+    elif form_select == "no_emojis":
+        return "Do not use emojis"
+    else:
+        raise ValueError()
+
+def style_prompt(form_select):
+    if form_select == "medium":
+        return "Keep it concise, but elaborate when necessary."
+    elif form_select == "detailed":
+        return "Describe the changes in detail and elaborate"
+    elif form_select == "brief":
+        return "Keep it brief and concise. Cover everything, but don't elaborate."
+    else:
+        raise ValueError()
--- pr_manager/urls.py
+++ pr_manager/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.home, name="pr_manager_home"),
-    path("<str:owner>/<str:repo>/<int:pr_number>/", views.view_pull_requests, name="view_pull_requests"),
+    path("<str:owner>/<str:repo>/<int:pr_number>/", views.view_pull_request, name="view_pull_request"),
     path("generate-description/", views.generate_description, name="generate_description"),
+    path("start-over/", views.start_over, name="start_over"),
 ]
